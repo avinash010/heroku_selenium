9bd1f6559ad94376f52d33a6f40e40fe4d3e75e6
Avinash <avinash@qxf2.com>
2018-11-26 11:58:03 +0530

    Load whats app page image

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
-from flask import Flask
+# Script to load whatsapp web and load the page using selenium and Flask
+import os, time
+from flask import Flask, send_file
 from selenium import webdriver
 app = Flask(__name__)
 
  
 @app.route("/")
 def hello():
+    # Load chrome driver
     driver = webdriver.Chrome()
     driver.get("https://web.whatsapp.com/")
-    title_text = driver.title
-    return title_text
+    time.sleep(10)
+    # Save the screenshot and load the image
+    screenshot_name = os.path.abspath(os.path.join(os.path.dirname(__file__),'screenshot.gif'))
+    driver.save_screenshot(screenshot_name)
+    return send_file('screenshot.gif', mimetype='image/gif')
  
 if __name__ == "__main__":
     app.run()
